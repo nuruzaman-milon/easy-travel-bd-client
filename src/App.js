c2f4fd12cf6cac1f1b2cb51aa38f7c0a6b42e0dc
Saqib Ahmad <you@example.com>
2022-12-17 12:28:06 +0600

    flowbite-react tailwind installed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Saqib commited a change</h2>
+    <div>
+
     </div>
   );
 }
