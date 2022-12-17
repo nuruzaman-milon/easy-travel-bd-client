f609f8589a45e22d2107d6caaa4cb2b208d890cc
Njmilon <njmilon1@gmail.com>
2022-12-17 21:07:55 +0600

    navbar added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
+import { Button } from 'flowbite-react';
 import React from 'react';
 
 const Home = () => {
     return (
         <div>
             {/* Add the components one by one */}
-            
+            <h1 className='text-red'>testing color</h1>
+            <Button color='red'>click here</Button>
         </div>
     );
 };
--- src/pages/Shared/Navbar/Header.js
+++ src/pages/Shared/Navbar/Header.js
@@ -1,9 +1,58 @@
+import { Button, Navbar } from 'flowbite-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Head</h1>
+        <div className='w-[90%] mx-auto'>
+            <Navbar
+                fluid={true}
+                rounded={true}
+            >
+                <Link to='/'>
+                    <Navbar.Brand >
+                        <img
+                            src="https://flowbite.com/docs/images/logo.svg"
+                            className="mr-3 h-6 sm:h-9"
+                            alt="Flowbite Logo"
+                        />
+                        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                            Flowbite
+                        </span>
+                    </Navbar.Brand>
+                </Link>
+
+                <div className="flex md:order-2">
+                    <Button color="success">
+                        Login/Signup
+                    </Button>
+                    <Navbar.Toggle />
+                </div>
+
+                <Navbar.Collapse>
+                    <Navbar.Link
+                        href="/navbars"
+                        active={true}
+                    >
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        About Us
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Our Services
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Query
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Contact Us
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Cancel Tickets
+                    </Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     );
 };
