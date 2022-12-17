f8a9a2db228a0ed8dd27b6efc06d0e3edf2a7fee
Saqib Ahmad <you@example.com>
2022-12-17 20:58:43 +0600

    search form added

---
+++ src/pages/Home/OurAmenities/OurAmenities.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OurAmenities = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default OurAmenities;
---
+++ src/pages/Home/PaymentSystems/PaymentSystems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentSystems = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default PaymentSystems;
