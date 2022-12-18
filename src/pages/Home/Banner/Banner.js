35961d1ae73110485081704813c4c607f8b81dec
moutosi-raka <raka29077997@gmail.com>
2022-12-18 12:22:09 +0600

    added text in banner

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -15,7 +15,7 @@ const Banner = () => {
               <img src='https://i.ibb.co/Pczch5H/bus3.jpg' alt='' />
             </div>
             <div className='text absolute top-1/2 left-1/ 2 text-white'>
-              <h1 className='text-3xl'>Bus Ticket</h1>
+              <h1 className='text-5xl font-extrabold'>Easy Travel BD</h1>
             </div>
 
           </div>
@@ -23,16 +23,16 @@ const Banner = () => {
             <div className='banner-1'>
               <img src='https://i.ibb.co/SX2ZVNt/bus6.jpg' alt='' />
             </div>
-            <div className='text absolute top-1/2 left-1/ 2 text-white'>
-              <h1 className='text-3xl'>Bus Ticket</h1>
+            <div className='text absolute top-1/2 letf-1/2 text-white'>
+              <h1 className='text-4xl font-extrabold text-center '>Best & Most  Professional Travel <br/> Option In Bangladesh</h1>
             </div>
           </div>
           <div className="flex h-full items-center justify-center relative">
             <div className='banner-1 '>
-              <img src='https://i.ibb.co/SX2ZVNt/bus6.jpg' alt='' />
+              <img src='https://i.ibb.co/Ws5wQ2R/bus7.jpg' alt='' />
             </div>
             <div className='text absolute top-1/2 left-1/ 2 text-white'>
-              <h1 className='text-3xl'>Bus Ticket</h1>
+              <h1 className='text-4xl font-extrabold text-center '>We have the widest range of <br/> luxury inter-city bus.</h1>
             </div>
           </div>
         </Carousel>
