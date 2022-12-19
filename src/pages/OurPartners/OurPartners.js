502c47f6a17462847f0f3c3f53b7a220ef6dfbd4
Khubaibul Islam <khubaibulislam2001@gmail.com>
2022-12-19 18:49:53 +0600

    Our Partners Section Complete

--- src/pages/OurPartners/OurPartners.js
+++ src/pages/OurPartners/OurPartners.js
@@ -1,9 +1,132 @@
 import React from 'react';
+import road from "../../assets/icons/road.png"
 
 const OurPartners = () => {
     return (
-        <div>
-            
+        <div className='mt-10'>
+            <h1 className='text-center font-bold text-primary text-3xl'>OUR BUS PARTNERS</h1>
+            <div>
+                <div className='lg:flex md:flex bg-primary justify-between items-center rounded my-14'>
+                    <div className='lg:p-4 w-full'>
+                        <h1 className='font-bold lg:text-3xl md:text-2xl sm:text-xl uppercase mb-4 text-center'>Green Line Paribahan</h1>
+                        <div className='flex justify-evenly'>
+                            <div>
+                                <h3 className='lg:text-xl font-semibold mb-2'>Our Roads</h3>
+                                <div>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Chittagong</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Chittagong - Dhaka</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Cox's Bazar</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Cox's Bazar - Dhaka</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Benapole</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Benapole - Dhaka</p>
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className='lg:text-xl font-semibold mb-2'>Our Facilities</h3>
+                                <div>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Free Buffet</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Live TV</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Onboard Free WiFi</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Friendly Guide</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Dual AC</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Blanket</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <img className='lg:w-2/5 md:w-2/5 w-full h-full lg:rounded-r md:rounded-r rounded-b' src="https://i.ibb.co/2hvZydh/green-line.jpg" alt="" />
+                </div>
+                <div className='lg:flex md:flex bg-primary justify-between items-center rounded mb-14'>
+                    <img className='lg:w-2/5 md:w-2/5 w-full h-full lg:rounded-l md:rounded-l rounded-t' src="https://i.ibb.co/VCPvcsW/hanif-enterprise.jpg" alt="" />
+                    <div className='lg:p-4 w-full'>
+                        <h1 className='font-bold lg:text-3xl md:text-2xl sm:text-xl uppercase mb-4 text-center'>Hanif Enterprise</h1>
+                        <div className='flex justify-evenly'>
+                            <div>
+                                <h3 className='lg:text-xl font-semibold mb-2'>Our Roads</h3>
+                                <div>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Chittagong</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Chittagong - Dhaka</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Cox's Bazar</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Cox's Bazar - Dhaka</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Benapole</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Benapole - Dhaka</p>
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className='lg:text-xl font-semibold mb-2'>Our Facilities</h3>
+                                <div>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Free Buffet</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Live TV</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Onboard Free WiFi</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Friendly Guide</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Dual AC</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Blanket</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='lg:flex md:flex bg-primary justify-between items-center rounded mb-14'>
+                    <div className='lg:p-4 w-full'>
+                        <h1 className='font-bold lg:text-3xl md:text-2xl sm:text-xl uppercase mb-4 text-center'>Saint Martin Hyundai</h1>
+                        <div className='flex justify-evenly'>
+                            <div>
+                                <h3 className='lg:text-xl font-semibold mb-2'>Our Roads</h3>
+                                <div>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Chittagong</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Chittagong - Dhaka</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Cox's Bazar</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Cox's Bazar - Dhaka</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Benapole</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Benapole - Dhaka</p>
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className='lg:text-xl font-semibold mb-2'>Our Facilities</h3>
+                                <div>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Free Buffet</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Live TV</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Onboard Free WiFi</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Friendly Guide</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Dual AC</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Blanket</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <img className='lg:w-2/5 md:w-2/5 w-full h-full lg:rounded-r md:rounded-r rounded-b' src="https://i.ibb.co/k182v4v/saint-martin-hunday.jpg" alt="" />
+                </div>
+                <div className='lg:flex md:flex bg-primary justify-between items-center rounded mb-14'>
+                    <img className='lg:w-2/5 md:w-2/5 w-full h-full lg:rounded-l md:rounded-l rounded-t' src="https://i.ibb.co/Bqv4SGN/sohag-elite.jpg" alt="" />
+                    <div className='lg:p-4 w-full'>
+                        <h1 className='font-bold lg:text-3xl md:text-2xl sm:text-xl uppercase mb-4 text-center'>Shohag Enterprise</h1>
+                        <div className='flex justify-evenly'>
+                            <div>
+                                <h3 className='lg:text-xl font-semibold mb-2'>Our Roads</h3>
+                                <div>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Chittagong</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Chittagong - Dhaka</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Cox's Bazar</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Cox's Bazar - Dhaka</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Dhaka - Benapole</p>
+                                    <p className='flex items-center gap-x-2'><img className='w-5' src={road} alt="" />Benapole - Dhaka</p>
+                                </div>
+                            </div>
+                            <div>
+                                <h3 className='lg:text-xl font-semibold mb-2'>Our Facilities</h3>
+                                <div>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Free Buffet</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Live TV</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Onboard Free WiFi</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Friendly Guide</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Dual AC</p>
+                                    <p className='flex items-center'><span className='text-xs'>⚫</span>Blanket</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
