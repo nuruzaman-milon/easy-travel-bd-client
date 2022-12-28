d1b404b21a41c80957fd91e5971356e5f2cce225
moutosi-raka <raka29077997@gmail.com>
2022-12-28 15:28:49 +0600

    added ticket buy and cencal section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
-import { Toaster } from 'react-hot-toast';
-=======
 import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { useDispatch } from 'react-redux';
->>>>>>> da2cd6350297899478c4cd5d8e62d749349c8dc7
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { auth } from './firebase/firebase.config';
@@ -13,14 +9,6 @@ import { setLogin, setLogOut } from './redux/userSlice';
 import { router } from './routes/Routes/Routes';
 
 function App() {
-<<<<<<< HEAD
-  return (
-    <>
-      <RouterProvider router={router}>
-
-      </RouterProvider>
-      <div><Toaster /></div>
-=======
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -41,7 +29,6 @@ function App() {
       <RouterProvider router={router}>
       </RouterProvider>
       <Toaster />
->>>>>>> da2cd6350297899478c4cd5d8e62d749349c8dc7
     </>
   );
 }
--- src/pages/Home/SearchForm/HowToCancelModal.js
+++ src/pages/Home/SearchForm/HowToCancelModal.js
@@ -1,5 +1,8 @@
 import { Modal } from 'flowbite-react';
 import React, { useState } from 'react';
+import pic1 from '../../../assets/cencal ticket/cancel-1.png'
+import pic2 from '../../../assets/cencal ticket/cancel-2.png'
+import './searchFrom.css'
 
 const HowToCancelModal = () => {
     const [showCancelModal, setShowCancelModal] = useState(false);
@@ -17,13 +20,38 @@ const HowToCancelModal = () => {
                     How to Cancel Ticket
                 </Modal.Header>
                 <Modal.Body>
-                    <div className="space-y-6">
-                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                        </p>
-                        <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                            The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                        </p>
+                <div className="space-y-6 text-[#555]">
+                       {/* step 1 */}
+                        <div className='mt-5 pl-2'>
+                            <div className='headline'>
+                            <h2 className='text-2xl font-semibold'>Step 1</h2>
+                            
+                            </div>
+                            <p className='mt-3'>1. Select Ticket Operation tab.
+                            </p>
+                            <p>2. Enter ticket PNR and click on Search then you will get ticket information / details.</p>
+                            
+                            <img className='my-3 ml-2' src={pic1} alt=''/>
+                        </div>
+
+                        {/* step 2 */}
+                        <div className='mt-5 pl-2'>
+                            <div className='headline'>
+                            <h2 className='text-2xl font-semibold'>Step 2</h2>
+                            
+                            </div>
+                            <p className='mt-3'>1. From ticket information / details you will see Cancel Ticket button if the ticket is eligible for cancellation.
+                            </p>
+                            <p>2. Read terms and conditions and click on chekbox to agree.</p>
+                            <img className='my-3 ml-2' src={pic2} alt=''/>
+                            <p className='my-2'>3. Press Cancel Ticket and Refund button to proceed.</p>
+                        </div>
+                       
+                        {/* step 4 */}
+                        <div className='mt-5 pl-2'>
+                        <p className='text-center my-2'>Refunds will be processed automatically at the time of cancellation. Depending on the customer’s Bank/Card/M-Cash, it may take 3-14 business days to reflect the credit in your account.</p>
+                        </div>
+                      
                     </div>
                 </Modal.Body>
             </Modal>
